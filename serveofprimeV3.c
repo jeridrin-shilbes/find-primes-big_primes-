4919222de63683208b3648b5348471346ed1d933
Jeridrin Shilbes <jeridrinshilbesforever@gmail.com>
2025-05-12 19:15:00 +0530

    finally works and now run same as V2

--- serveofprimeV3.c
+++ serveofprimeV3.c
@@ -4,30 +4,31 @@
 
 int main()
 {
-    const int a=10000;
-    bool **num = malloc(a * sizeof(bool));  
+    const int a=1000000;
+    bool **num = malloc(a * sizeof(bool *));  
     
-    if (*num == NULL) {
-        printf("Memory allocation failed\n");
+    if (num == NULL) {
+        printf("Memory allocation failed:1\n");
         return 1;  
     }
     double tpn=0; //total primes counted
 
     //loop to initialise "a" bools
     for (int i = 0; i < a; i++) {
+        num[i]=malloc(sizeof(bool));
         *num[i] = true;
     }
 
     //set the prime indexes to false
     for (int i=1; i*i<a;i++)
     {
-        if(num[i])
+        if(*num[i])
         {
             int j=i+1;
             int current=i+j;
             while( (current<a) )
             {
-                free(num[current]);
+                *num[current]=false;
                 current+=j;
             }
         }
@@ -46,7 +47,7 @@ int main()
         //checking if line has a prime
         for(int j=i*10;j<(i*10)+10;j+=2)
         {
-            if(num[j])
+            if(*num[j])
             {
                 prime_found=true;
                 break;
@@ -56,7 +57,7 @@ int main()
         if (!prime_found)printf("\033[42m\033[37m");//white numbers in green background
         for(int j=i*10;j<(i*10)+10;j++)
         {
-            if(num[j]) {printf("\033[31m%i \033[32m",j+1);tpn++;}
+            if(*num[j]) {printf("\033[31m%i \033[32m",j+1);tpn++;}
             else printf("%i ",j+1);
         }
 
@@ -68,8 +69,12 @@ int main()
         else cnp=0;
         printf("\033[0m\n");
     }
-
-    free(num);  
+ 
+    for(int i=0;i<a;i++)
+    {
+        free(num[i]);
+    }
+    free(num); 
     printf("V2:\nThe total number of \n");
     printf("                     ");
     printf("primes counted       : %.0f\n",tpn);
