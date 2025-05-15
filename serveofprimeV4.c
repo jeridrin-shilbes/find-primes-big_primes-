274f12998d5eb371ca0b64c4d2d3e74f80b266ec
Jeridrin Shilbes <jeridrinshilbesforever@gmail.com>
2025-05-15 21:10:20 +0530

    included long long for all the variables that will hld more than an int

--- serveofprimeV4.c
+++ serveofprimeV4.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-void print(bool *arr,int a,int b);
+void print(bool *arr,long long a,long long b);
 int total_primes=0;
 int main()
 {
-    double a=1000000;
+    long long a=10000000000;
     double sqrt_a=sqrt(a);
     bool *num=malloc(sqrt_a * sizeof(bool));
     if(num==NULL)
@@ -40,19 +40,19 @@ int main()
     }
     print(num,0,sqrt_a);
  
-    int last=sqrt_a;
+    long long last=sqrt_a;
     while(last<a)
     {
         // printf("loop_started\n");
-        int end=(a-last>sqrt_a)?last+sqrt_a:a;
+        long long end=(a-last>sqrt_a)?last+sqrt_a:a;
         memset(part_num,1,sqrt_a*sizeof(bool));
         for(int i=1;i<sqrt_a;i++)
         {
             if(num[i])
             {
                 int j=i+1;
-                int start=((last+j-1)/j)*j;
-                for(int k=start;k<=end;k+=j)
+                long long start=((last+j-1)/j)*j;
+                for(long long k=start;k<=end;k+=j)
                 {
                     part_num[k-last-1]=false;
                 }
@@ -69,20 +69,20 @@ int main()
     printf("\nTotal primes: %i",total_primes);
 }
 
-void print(bool *arr,int start,int end)
+void print(bool *arr,long long start,long long end)
 {
     // printf("print_started_from_%d_to_%d\n",start,end);
-    int n=end-start;
+    long long n=end-start;
     for(int i=0;i<n;i++)
     {
         if(*(arr+i))
         {
             total_primes++;
-            printf("\033[31m%d",start+i+1);
+            printf("\033[31m%lld",start+i+1);
         }
         else
         {
-            printf("\033[32m%d",start+i+1);
+            printf("\033[32m%lld",start+i+1);
         }
         if((start+1+i)%10)
         {
